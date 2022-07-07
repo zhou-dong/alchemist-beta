359b489e79078d5a1a9a40af6dc5cf65e87a7dfd
Dong Zhou <dozhou@rubiconproject.com>
2022-07-07 02:55:24 -0700

    Create sphere.ts

---
+++ src/commons/three/sphere.ts
@@ -0,0 +1,23 @@
+import * as THREE from 'three';
+import { ColorRepresentation } from 'three';
+
+export default class {
+
+    private scene: THREE.Scene;
+    private mesh: THREE.Mesh;
+
+    constructor(
+        radius: number,
+        color: ColorRepresentation,
+        scene: THREE.Scene
+    ) {
+        const geometry = new THREE.SphereGeometry(radius);
+        const material = new THREE.MeshBasicMaterial({ color });
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.scene = scene;
+    }
+
+    public hide() {
+        this.scene.remove(this.mesh);
+    }
+}
